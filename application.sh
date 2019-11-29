c147aa2c2fbb2e9c155b2069e300dc4a8a8ee81d
AlejandroMu <alejandrobravo0329@gmail.com>
2019-11-29 10:52:32 -0500

    delete java path

--- application.sh
+++ application.sh
@@ -2,10 +2,9 @@
 echo 'Ingrese un puerto disponible'
 read PORT
 
-JAVA_DIR='/usr/java/jdk1.8.0_171-amd64/bin/'
-
-$JAVA_DIR/java -jar target/manager-0.0.1-SNAPSHOT.jar --server.port=$PORT &
+java -jar target/manager-0.0.1-SNAPSHOT.jar --server.port=$PORT &
 echo 'Espere :v'
 echo 'Por favor no se olvide de matar el proceso en este puerto, con kill -9 $(lsof -t -i:'$PORT')'
 sleep 5s
 echo 'Ingrese a: ' http://localhost:$PORT/process
+firefox http://localhost:$PORT/process
